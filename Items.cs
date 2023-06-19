1a77bbfa3b2b6ef9b6a37766901fbd8cc6d2ce0c
Salacious <chrisjenda@gmail.com>
2023-06-18 17:10:18 -0700

    Add project files.

---
+++ Items.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace HS_EquipInWater
+{
+    class Items
+    {
+        public List<string> allowAxes = new List<string>(new string[] {
+            "$item_axe_blackmetal",
+            "$item_axe_bronze",
+            "$item_axe_flint",
+            "$item_axe_iron",
+            "$item_axe_stone",
+            "$item_battleaxe",
+            "$item_battleaxe_crystal"});
+
+        public List<string> allowBows = new List<string>(new string[] {
+            "$item_bow",
+            "$item_bow_draugrfang",
+            "$item_bow_finewood",
+            "$item_bow_huntsman"});
+
+        public List<string> allowAtgeirs = new List<string>(new string[] {
+            "$item_atgeir_blackmetal",
+            "$item_atgeir_bronze",
+            "$item_atgeir_iron"});
+
+        public List<string> allowKnives = new List<string>(new string[] {
+            "$item_knife_blackmetal",
+            "$item_knife_butcher",
+            "$item_knife_chitin",
+            "$item_knife_copper",
+            "$item_knife_flint",
+            "$item_knife_silver"});
+
+        public List<string> allowMaces = new List<string>(new string[] {
+            "$item_mace_bronze",
+            "$item_mace_iron",
+            "$item_mace_silver",
+            "$item_mace_needle"});
+
+        public List<string> allowShields = new List<string>(new string[] {
+            "$item_shield_banded",
+            "$item_shield_blackmetal",
+            "$item_shield_blackmetal_tower",
+            "$item_shield_bronzebuckler",
+            "$item_shield_iron_square",
+            "$item_shield_iron_tower",
+            "$item_shield_serpentscale",
+            "$item_shield_silver",
+            "$item_shield_wood",
+            "$item_shield_woodtower",
+            "$item_shield_bonetower",
+            "$item_shield_ironbuckler"});
+
+        public List<string> allowSledge = new List<string>(new string[] {
+            "$item_sledge_iron"});
+
+        public List<string> allowSpears = new List<string>(new string[] {
+            "$item_spear_bronze",
+            "$item_spear_chitin",
+            "$item_spear_ancientbark",
+            "$item_spear_flint",
+            "$item_spear_wolffang"});
+
+        public List<string> allowSwords = new List<string>(new string[] {
+            "$item_sword_blackmetal",
+            "$item_sword_bronze",
+            "$item_sword_iron",
+            "$item_sword_fire",
+            "$item_sword_silver"});
+
+        public List<string> allowClub = new List<string>(new string[] {
+            "$item_club"});
+
+        public List<string> allowTorch = new List<string>(new string[] {
+            "$item_torch"});
+
+        public List<string> allowPickaxes = new List<string>(new string[] {
+            "$item_pickaxe_antler",
+            "$item_pickaxe_bronze",
+            "$item_pickaxe_iron",
+            "$item_pickaxe_stone"});
+
+        public List<string> allowCultivator = new List<string>(new string[] {
+            "$item_cultivator"});
+
+        public List<string> allowFishingRod = new List<string>(new string[] {
+            "$item_fishingrod"});
+
+        public List<string> allowHammer = new List<string>(new string[] {
+            "$item_hammer"});
+
+        public List<string> allowHoe = new List<string>(new string[] {
+            "$item_hoe"});
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,148 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using System.Linq;
+using OpCodes = System.Reflection.Emit.OpCodes;
+
+namespace HS_EquipInWater
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class HS_EquipInWater : BaseUnityPlugin
+    {
+        private const string ModName = "HS_EquipInWater";
+        private const string ModVersion = "1.0.0";
+        private const string ModGUID = "hs_equipinwater";
+
+        public static readonly ManualLogSource MlLogger = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+        private List<ConfigEntry<bool>> configEntries = new();
+        private static List<string> deniedItems = new();
+        private Items itemList = new();
+
+        void Awake()
+        {
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowAxes", true, "Allow axes to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowBows", true, "Allow bows to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowAtgeirs", true, "Allow atgeirs to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowKnives", true, "Allow knives to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowMaces", true, "Allow maces to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowShields", true, "Allow shields to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowSledge", true, "Allow sledge to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowSpears", true, "Allow spears to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowSwords", true, "Allow swords to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowClub", true, "Allow club to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowTorch", false, "Allow torch to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowPickaxes", true, "Allow pickaxes to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowCultivator", true, "Allow cultivator to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowFishingRod", true, "Allow fishing rod to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowHammer", true, "Allow hammer to be used in Water"));
+            configEntries.Add(Config.Bind("ItemsToAllow", "allowHoe", true, "Allow hoe to be used in Water"));
+
+            foreach (ConfigEntry<bool> confEntry in configEntries)
+            {
+                if (!confEntry.Value)
+                {
+                    List<string> stringList = itemList.GetType().GetField(confEntry.Definition.Key).GetValue(itemList) as List<string>;
+                    foreach (string a in stringList)
+                    {
+                        deniedItems.Add(a);
+                    }
+                }
+            }
+            Harmony harmony = new Harmony(ModGUID);
+
+            harmony.Patch(
+                AccessTools.Method(typeof(Player), "Update"),
+                transpiler: new HarmonyMethod(typeof(HS_EquipInWaterPatches), nameof(HS_EquipInWaterPatches.HS_PatchPlayerUpdateWaterCheck))
+            );
+
+            harmony.Patch(
+                AccessTools.Method(typeof(Humanoid), "EquipItem"),
+                transpiler: new HarmonyMethod(typeof(HS_EquipInWaterPatches), nameof(HS_EquipInWaterPatches.HS_InjectWaterItemCheck))
+            );
+
+            harmony.Patch(
+                AccessTools.Method(typeof(Humanoid), "UpdateEquipment"),
+                transpiler: new HarmonyMethod(typeof(HS_EquipInWaterPatches), nameof(HS_EquipInWaterPatches.HS_PatchFixedUpdatedWaterCheck))
+            );
+
+            // Example of How to Patch Protected Methods for LVH-IT, in case they ever read this.
+            // harmony.Patch(AccessTools.Method(typeof(Humanoid), "HideHandItems"), prefix: new HarmonyMethod(typeof(HS_EquipmentWaterFix), nameof(PatchHideHandItems)));
+        }
+
+        // Example of How to Patch Protected Methods for LVH-IT, in case they ever read this.
+        //private static void PatchHideHandItems(Humanoid __instance, ref bool __runOriginal)
+        //{
+        //    __runOriginal = false;
+
+        //    if (__instance.m_leftItem == null && __instance.m_rightItem == null)
+        //    {
+        //        return;
+        //    }
+
+        //    ItemDrop.ItemData leftItem = __instance.m_leftItem;
+        //    ItemDrop.ItemData rightItem = __instance.m_rightItem;
+        //    __instance.UnequipItem(__instance.m_leftItem, true);
+        //    __instance.UnequipItem(__instance.m_rightItem, true);
+        //    __instance.m_hiddenLeftItem = leftItem;
+        //    __instance.m_hiddenRightItem = rightItem;
+        //    __instance.SetupVisEquipment(__instance.m_visEquipment, false);
+        //    __instance.m_zanim.SetTrigger("equip_hip");
+
+        //}
+
+        public static bool HS_CheckWaterItem(ItemDrop.ItemData item)
+        {
+            
+            return deniedItems.Contains(item.m_shared.m_name);
+        }
+
+        public static class HS_EquipInWaterPatches
+        {
+            // Remove (Is in Water) check for when Player uses Hide Key
+            public static IEnumerable<CodeInstruction> HS_PatchPlayerUpdateWaterCheck(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> instructionList = instructions.ToList();
+
+                for (int i = 202; i <= 207; i++)
+                {
+                    CodeInstruction instruction = instructionList[i];
+                    instruction.opcode = OpCodes.Nop;
+                }
+
+                return instructionList;
+            }
+
+
+            // Inject a Call to our custom function "HS_CheckWaterItem" within the If block to check if Item is Permissable to be used in water.
+            public static IEnumerable<CodeInstruction> HS_InjectWaterItemCheck(IEnumerable<CodeInstruction> instructions)
+            {
+                var instructionList = new List<CodeInstruction>(instructions);
+                var injectionInstructions = new List<CodeInstruction>
+                {
+                    new (OpCodes.Ldarg_1),
+                    new (OpCodes.Call, typeof(HS_EquipInWater).GetMethod("HS_CheckWaterItem")),
+                    new (OpCodes.Brfalse_S, instructionList[29].operand) // Reuse Label from Instruction 29 for JMP
+                };
+
+                instructionList.InsertRange(33, injectionInstructions);
+                return instructionList;
+            }
+
+            // Remove (Is in Water) check during Humanoid Fixed Update
+            public static IEnumerable<CodeInstruction> HS_PatchFixedUpdatedWaterCheck(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> instructionList = instructions.ToList();
+
+                for (int i = 4; i <= 11; i++)
+                {
+                    CodeInstruction instruction = instructionList[i];
+                    instruction.opcode = OpCodes.Nop;
+                }
+
+                return instructionList;
+            }
+        }
+    }
+}
